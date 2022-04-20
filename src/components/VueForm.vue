56f838a8cabc07ac3c21bcba876e163d74b7e3c3
AlexandrBartkiv <alexandrbartkiv@gmail.com>
2022-04-21 00:26:58 +0300

    Db and pages created

--- src/components/VueForm.vue
+++ src/components/VueForm.vue
@@ -1,21 +1,37 @@
 <template>
-    <div>
-        <form class="Form">
-            <div class="inpText">
-                <input type="text" name="" id="">
-                <input type="mail" name="" id="">
-                <input type="tel" name="" id="">
-            </div>
-            <div class="inpRadio">
-                <input type="radio" name="front" id="">
-                <label for="front">Front</label>
-                <input type="radio" name="" id="">
-                <input type="radio" name="" id="">
-                <input type="radio" name="" id="">
-            </div>
-            
-        </form>
-    </div>
+    <div class="q-pa-md" style="width: 328px">
+
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        outlined
+        v-model="name"
+        label="Your name"
+        hint="Name and surname"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+      />
+
+      <q-input
+        outlined
+        type="email"
+        v-model="age"
+        label="Email"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+      />
+      <div>
+        
+      </div>
+    </q-form>
+
+  </div>
 </template>
 <script>
 export default {
--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "normalize.css"
 import './styles/styles.css'
-createApp(App).mount('#app')
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
+createApp(App).use(Quasar, quasarUserOptions).mount('#app')
